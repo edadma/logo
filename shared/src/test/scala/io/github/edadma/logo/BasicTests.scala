cd0a7adb3ca09f8c95a6770089ee91cdcd026ada
Edward A Maxedon <edadma@gmail.com>
2024-11-02 21:17:59 -0400

    code unit tests

--- shared/src/test/scala/io/github/edadma/logo/BasicTests.scala
+++ shared/src/test/scala/io/github/edadma/logo/BasicTests.scala
@@ -17,11 +17,23 @@ class BasicTests extends AnyFreeSpec with Matchers with Test {
     an[Exception] should be thrownBy eval("123x")
   }
 
+  "literal 4" in {
+    eval("[a b]") shouldBe "a b"
+  }
+
+  "literal 5" in {
+    eval("[a [b] c]") shouldBe "a [b] c"
+  }
+
   "print 1" in {
     run("print 123") shouldBe "123"
   }
 
   "print 2" in {
     run("print \"123x") shouldBe "123x"
+  }
+
+  "print 3" in {
+    an[Exception] should be thrownBy run("print 123x")
   }
 }
