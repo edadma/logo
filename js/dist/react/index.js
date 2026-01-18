997bedf0147b2a8cefbcaafee04b19ad8e88c9dd
Edward A Maxedon <edadma@gmail.com>
2026-01-18 00:58:29 -0500

    Fix npm build and add gitignore
    - Fix TypeScript re-export syntax in react/index.ts
    - Add .gitignore to exclude node_modules
    - Add compiled React component and type definitions

---
+++ js/dist/react/index.js
@@ -0,0 +1,2 @@
+export { LogoCanvas } from './LogoCanvas';
+export { LogoCanvas as default } from './LogoCanvas';
--- js/react/index.ts
+++ js/react/index.ts
@@ -1,2 +1,2 @@
 export { LogoCanvas, type LogoCanvasProps, type LogoCanvasRef } from './LogoCanvas';
-export default LogoCanvas;
+export { LogoCanvas as default } from './LogoCanvas';
